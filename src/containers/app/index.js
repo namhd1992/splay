facefd773e10b85b1cf39e120058b3431da4ca2e
stpnghd <buivannam1992@gmail.com>
2019-01-08 10:10:21 +0700

    fix

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -36,7 +36,8 @@ import Avatar from 'material-ui/Avatar';
 import Phone_card from '../phone_card';
 import Coin from '../coin';
 import TypeChangeCoin from '../type_change_coin';
-import SelectGame from '../select_game'
+import SelectGame from '../select_game';
+import EventGame from '../event_game';
 
 
 class App extends React.Component {
@@ -47,6 +48,7 @@ class App extends React.Component {
 			main: null,
 			compact: false,
 			scrolling: false,
+			backgroundColor:'#212933',
 			fullscreen: false,
 			title: "",
 			isMobile: false,
@@ -63,8 +65,8 @@ class App extends React.Component {
 			this.setState({ isMobile: true });
 		}
 		window.addEventListener('scroll', this.handleScroll);
-		if (document.location.pathname === "/giftcodepluginlogin" || document.location.pathname === "/giftcodeplugin") {
-			this.setState({ fullscreen: true });
+		if (document.location.pathname === "/giftcodepluginlogin" || document.location.pathname === "/giftcodeplugin" || document.location.pathname==="/eventgame") {
+			this.setState({ fullscreen: true, backgroundColor:'#1a1a1a' });
 		} else {
 			this.setState({ fullscreen: false });
 		}
@@ -108,11 +110,11 @@ class App extends React.Component {
 	}
 	render() {
 		return (
-			<div style={{ backgroundColor: "#212933" }}>
-				<div style={{ maxWidth: "1280px", margin: "auto", background: "#212933" }}>
+			<div style={{ backgroundColor: this.state.backgroundColor }}>
+				<div style={{ maxWidth: "1280px", margin: "auto", background: this.state.backgroundColorc }}>
 					{(!this.state.fullscreen) ? (<MenuAppBar isMobile={this.state.isMobile} pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
 						data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>) : (<div></div>)}
-					<main ref={(c) => this.main = c} style={(document.location.pathname === "/") ? { padding: "60px 8px 8px 8px" } : { padding: "60px 8px 8px 8px" }}>
+					<main ref={(c) => this.main = c} style={(document.location.pathname === "/eventgame") ? { padding: "30px 8px 8px 8px" } : { padding: "60px 8px 8px 8px" }}>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/about-us" component={About} />
 						<Route exact path="/loginwidget" component={LoginWidget} />
@@ -141,6 +143,7 @@ class App extends React.Component {
 						<Route exact path="/giftcodepluginlogin" component={Giftcode_plugin_login} />
 						<Route exact path="/phonecard" component={Phone_card} />
 						<Route exact path="/chongame" component={SelectGame} />
+						<Route exact path="/eventgame" component={EventGame} />
 						<Route exact path="/chitiet" component={Coin} />
 						<Route exact path="/doi" component={TypeChangeCoin} />
 					</main>
